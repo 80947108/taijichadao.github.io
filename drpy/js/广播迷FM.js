c3822ea5226d2ae1d406447bf4ad85e06e971af8
taijichadao <88364460+taijichadao@users.noreply.github.com>
2023-05-27 17:57:12 +0800

    Update 广播迷FM.js

--- drpy/js/广播迷FM.js
+++ drpy/js/广播迷FM.js
@@ -1,5 +1,5 @@
 var rule = {
-    title:'广播',
+    title:'广播迷',
     编码:'gb18030',
     host:'http://www.guangbomi.com',
     url:'/fyclass/fypage.html[/fyclass]',
@@ -11,8 +11,8 @@ var rule = {
     },
     timeout:5000,
 
-    class_name:'中央FM2&中央FM&北京&浙江&湖南&央视TV&湖南TV',
-    class_url:'fmlist223.html&live/zy&live/bj&live/zj&live/hunan&tv/zy&tv/hn',
+    class_name:'中央FM&湖南FM&北京FM&上海FM&天津FM&重庆FM&广东FM&四川FM&山东FM&江苏FM&浙江FM&河南FM&河北FM&辽宁FM&安徽FM&湖北FM&福建FM&广西FM&陕西FM&山西FM&江西FM&吉林FM&云南FM&贵州FM&西藏FM&宁夏FM&海南FM&新疆FM&甘肃FM&青海FM&黑龙江FM&内蒙古FM&央视TV&湖南TV',
+    class_url:'fmlist223.html&live/hunan&live/bj&live/sh&live/tj&live/cq&live/gd&live/sc&live/sd&live/js&live/zj&live/henan&live/hebei&live/ln&live/ah&live/hubei&live/fj&live/gx&live/sxi&live/sx&live/jx&live/jl&live/yn&live/gz&live/xz&live/nx&live/hainan&live/xj&live/gs&live/qh&live/hlj&live/nmg&tv/zy&tv/hn',
 	play_parse:true,
 	lazy:'js:var purl=jsp.pdfh(request(input), ".playcode&&iframe&&src");if(/tingtingfm/.test(purl)){purl="http://www.guangbomi.com"+purl};input= {jx:0,url:purl,parse:1,header:JSON.stringify({"referer":"http://www.guangbomi.com/"})}',
 	limit:6,
