07f0aa0a1f587d56527504f8c4b72485c8e0d3c6
taijichadao <88364460+taijichadao@users.noreply.github.com>
2023-12-12 21:19:19 +0800

    Add files via upload

--- py/py_cctv_lmdq.py
+++ py/py_cctv_lmdq.py
@@ -9,7 +9,7 @@ import base64
 
 class Spider(Spider):  # 元类 默认的元类 type
 	def getName(self):
-		return "央视"
+		return "央视大全"
 	def init(self,extend=""):
 		print("============{0}============".format(extend))
 		pass
@@ -20,102 +20,8 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def homeContent(self,filter):
 		result = {}
 		cateManual = {
-			  "新闻联播": "TOPC1451528971114112",
-  "焦点访谈": "TOPC1451558976694518",
-  "跟着书本去旅行": "TOPC1575253587571324",
-  "探索·发现": "TOPC1451557893544236",
-  "人与自然": "TOPC1451525103989666",
-  "地理·中国": "TOPC1451557421544786",
-  "科学动物园": "TOPC1571021385508957",
-  "经济半小时": "TOPC1451533652476962",
-  "今日说法": "TOPC1451464665008914",
-  "消费主张": "TOPC1451534775834896",
-  "中国经济大讲堂": "TOPC1514182710380601",
-  "对话": "TOPC1451530382483536",
-  "走遍中国": "TOPC1451542134053698",
-  "军事科技": "TOPC1451528087494889",
-  "百家讲坛": "TOPC1451557052519584",
-  "百家说故事": "TOPC1574995326079121",
-  "读书": "TOPC1451557523542854",
-  "智力快车": "TOPC1451559756374759",
-  "等着我": "TOPC1451378757637200",
-  "新闻直播间": "TOPC1451559129520755",
-  "海峡两岸": "TOPC1451540328102649",
-  "天网": "TOPC1451530382483536",
-  "今日关注": "TOPC1451540389082713",
-  "高端访谈": "TOPC1665739007799851",
-  "开讲啦": "TOPC1451464884159276",
-  "故事里的中国": "TOPC1451464884159276",
-  "面对面": "TOPC1451559038345600",
-  "动物世界": "TOPC1451378967257534",
-  "走进科学": "TOPC1451558190239536",
-  "远方的家": "TOPC1451541349400938",
-  "自然传奇": "TOPC1451558150787467",
-  "乡土中国": "TOPC1563178586782832",
-  "是真的吗": "TOPC1451534366388377",
-  "星光大道": "TOPC1451467630488780",
-  "健康之路": "TOPC1451557646802924",
-  "动画大放映": "TOPC1451559025546574",
-  "第一动画乐园": "TOPC1451378857272262",
-  "精彩音乐汇": "TOPC1451541414450906",
-  "音乐厅": "TOPC1451534421925242",
-  "民歌·中国": "TOPC1451541994820527",
-  "中国电影报道": "TOPC1451354597100320",
-  "星推荐": "TOPC1451469943519994",
-  "方圆剧阵": "TOPC1571217727564820",
-  "空中剧院": "TOPC1451558856402351",
-  "正大综艺": "TOPC1650782829200997",
-  "体坛快讯": "TOPC1451550970356385",
-  "第一时间": "TOPC1451530259915198",
-  "开门大吉": "TOPC1451465894294259",
-  "共同关注": "TOPC1451558858788377",
-  "军事报道": "TOPC1451527941788652",
-  "新闻调查": "TOPC1451558819463311",
-  "新闻30分": "TOPC1451559097947700",
-  "新闻1+1": "TOPC1451559066181661",
-  "今日亚洲": "TOPC1451540448405749",
-  "新闻直播间": "TOPC1451559129520755",
-  "24小时": "TOPC1451558428005729",
-  "中国新闻": "TOPC1451539894330405",
-  "午夜新闻": "TOPC1451558779639282",
-  "朝闻天下": "TOPC1451558496100826",
-  "晚间新闻": "TOPC1451528792881669",
-  "新闻周刊": "TOPC1451559180488841",
-  "正点财经": "TOPC1453100395512779",
-  "生活圈": "TOPC1451546588784893",
-  "生活提示": "TOPC1451526037568184",
-  "东方时空": "TOPC1451558532019883",
-  "经济信息联播": "TOPC1451533782742171",
-  "今日环球": "TOPC1571034705435323",
-  "一线": "TOPC1451543462858283",
-  "我爱发明": "TOPC1569314345479107",
-  "地理中国": "TOPC1451557421544786",
-  "非常6+1": "TOPC1451467940101208",
-  "中国节拍": "TOPC1570025984977611",
-  "一鸣惊人": "TOPC1451558692971175",
-  "金牌喜剧班": "TOPC1611826337610628",
-  "九州大戏台": "TOPC1451558399948678",
-  "乡村大舞台": "TOPC1563179546003162",
-  "家庭幽默大赛": "TOPC1451375222891702",
-  "综艺盛典": "TOPC1451985071887935",
-  "环球综艺": "TOPC1571300682556971",
-  "广场舞金曲": "TOPC1528685010104859",
-  "锦绣梨园": "TOPC1451558363250650",
-  "梨园周刊": "TOPC1574909786070351",
-  "外国人在中国": "TOPC1451541113743615",
-  "华人世界": "TOPC1451539822927345",
-  "武林大会": "TOPC1451551891055866",
-  "棋牌乐": "TOPC1451550531682936",
-  "动物传奇": "TOPC1451984181884527",
-  "美食中国": "TOPC1571034804976375",
-  "田间示范秀": "TOPC1563178908227191",
-  "三农群英会": "TOPC1600745974233265",
-  "乡村振兴面对面": "TOPC1568966531726705",
-  "超级新农人": "TOPC1597627647957699",
-  "印象乡村": "TOPC1563178734372977"
-
+			"央视大全": "CCTV"
 		}
-		
 		classes = []
 		for k in cateManual:
 			classes.append({
@@ -133,29 +39,41 @@ class Spider(Spider):  # 元类 默认的元类 type
 		return result
 	def categoryContent(self,tid,pg,filter,extend):		
 		result = {}
-		extend['id'] = tid
+		month = ""
+		year = ""
+		if 'month' in extend.keys():
+			month = extend['month']
+		if 'year' in extend.keys():
+			year = extend['year']
+		if year == '':
+			month = ''
+		prefix = year + month
 		extend['p'] = pg
-		filterParams = ["id", "p", "d"]
-		params = ["", "", ""]
-		for idx in range(len(filterParams)):
-			fp = filterParams[idx]
-			if fp in extend.keys():
-				params[idx] = '{0}={1}'.format(filterParams[idx],extend[fp])
-		suffix = '&'.join(params)
-		url = 'https://api.cntv.cn/NewVideo/getVideoListByColumn?{0}&n=20&sort=desc&mode=0&serviceId=tvcctv&t=json'.format(suffix)
-		if not tid.startswith('TOPC'):
-			url = 'https://api.cntv.cn/NewVideo/getVideoListByAlbumIdNew?{0}&n=20&sort=desc&mode=0&serviceId=tvcctv&t=json'.format(suffix)
-		rsp = self.fetch(url,headers=self.header)
-		jo = json.loads(rsp.text)
-		vodList = jo['data']['list']
+		filterMap = {
+			"fl":"",
+			"fc":"",
+			"cid":"",
+			"p":"1"
+		}
+		suffix = ""
+		for key in filterMap.keys():
+			if key in extend.keys():
+				filterMap[key] = extend[key]
+			suffix = suffix + '&' + key + '=' + filterMap[key]
+		url = 'https://api.cntv.cn/lanmu/columnSearch?{0}&n=20&serviceId=tvcctv&t=json'.format(suffix)
+		jo = self.fetch(url,headers=self.header).json()
+		vodList = jo['response']['docs']
 		videos = []
 		for vod in vodList:
-			guid = vod['guid']
-			title = vod['title']
-			img = vod['image']
-			brief = vod['brief']
+			lastVideo = vod['lastVIDE']['videoSharedCode']
+			if len(lastVideo) == 0:
+				lastVideo = '_'
+			guid = prefix+'###'+vod['column_name']+'###'+lastVideo+'###'+vod['column_logo']
+			# guid = prefix+'###'+vod['column_website']+'###'+vod['column_logo']
+			title = vod['column_name']
+			img = vod['column_logo']
 			videos.append({
-				"vod_id":guid+"###"+img,
+				"vod_id":guid,
 				"vod_name":title,
 				"vod_pic":img,
 				"vod_remarks":''
@@ -169,33 +87,48 @@ class Spider(Spider):  # 元类 默认的元类 type
 	def detailContent(self,array):
 		aid = array[0].split('###')
 		tid = aid[0]
-		url = "https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={0}".format(tid)
+		logo = aid[3]
+		lastVideo = aid[2]
+		title = aid[1]
+		date = aid[0]
+		if lastVideo == '_':
+			return {}
 
-		rsp = self.fetch(url,headers=self.header)
-		jo = json.loads(rsp.text)
-		title = jo['title'].strip()
-		link = jo['hls_url'].strip()
+		lastUrl = 'https://api.cntv.cn/video/videoinfoByGuid?guid={0}&serviceId=tvcctv'.format(lastVideo)
+		lastJo = self.fetch(lastUrl,headers=self.header).json()
+		topicId = lastJo['ctid']
+		url = "https://api.cntv.cn/NewVideo/getVideoListByColumn?id={0}&d={1}&p=1&n=100&sort=desc&mode=0&serviceId=tvcctv&t=json".format(topicId,date)
+		jo = self.fetch(url,headers=self.header).json()
+		vodList = jo['data']['list']
+		videoList = []
+		for video in vodList:
+			videoList.append(video['title']+"$"+video['guid'])
+		if len(videoList) == 0:
+			return {}
+		if len(date) == 0:
+			date = time.strftime("%Y", time.localtime(time.time()))
 		vod = {
-			"vod_id":tid,
-			"vod_name":title,
-			"vod_pic":aid[1],
-			"type_name":'',
-			"vod_year":"",
+			"vod_id":array[0],
+			"vod_name":date +" "+title,
+			"vod_pic":logo,
+			"type_name":lastJo['channel'],
+			"vod_year":date,
 			"vod_area":"",
-			"vod_remarks":"",
+			"vod_remarks":date,
 			"vod_actor":"",
-			"vod_director":"",
-			"vod_content":""
+			"vod_director":topicId,
+			"vod_content":"当前页面默认只展示最新100期的内容，可在分类页面选择年份和月份进行往期节目查看。年份和月份仅影响当前页面内容，不参与分类过滤。视频默认播放可以获取到的最高帧率。"
 		}
-		vod['vod_play_from'] = 'CCTV'
-		vod['vod_play_url'] = title+"$"+link
 
+		vod['vod_play_from'] = 'CCTV'
+		vod['vod_play_url'] = "#".join(videoList)
 		result = {
 			'list':[
 				vod
 			]
 		}
 		return result
+
 	def searchContent(self,key,quick):
 		result = {
 			'list':[]
@@ -203,11 +136,14 @@ class Spider(Spider):  # 元类 默认的元类 type
 		return result
 	def playerContent(self,flag,id,vipFlags):
 		result = {}
-		rsp = self.fetch(id,headers=self.header)
+		url = "https://vdn.apps.cntv.cn/api/getHttpVideoInfo.do?pid={0}".format(id)
+		jo = self.fetch(url,headers=self.header).json()
+		link = jo['hls_url'].strip()
+		rsp = self.fetch(link,headers=self.header)
 		content = rsp.text.strip()
 		arr = content.split('\n')
-		urlPrefix = self.regStr(id,'(http[s]?://[a-zA-z0-9.]+)/')
-		
+		urlPrefix = self.regStr(link,'(http[s]?://[a-zA-z0-9.]+)/')
+
 		subUrl = arr[-1].split('/')
 		subUrl[3] = '1200'
 		subUrl[-1] = '1200.m3u8'
@@ -227,10 +163,12 @@ class Spider(Spider):  # 元类 默认的元类 type
 
 	config = {
 		"player": {},
-		"filter": {"TOPC1451557970755294": [{"key": "d", "name": "年份", "value": [{"n": "全部", "v": ""}, {"n": "2024", "v": "2024"}, {"n": "2023", "v": "2023"}, {"n": "2022", "v": "2022"}, {"n": "2021", "v": "2021"}, {"n": "2020", "v": "2020"}, {"n": "2019", "v": "2019"}, {"n": "2018", "v": "2018"}, {"n": "2017", "v": "2017"}, {"n": "2016", "v": "2016"}, {"n": "2015", "v": "2015"}, {"n": "2014", "v": "2014"}, {"n": "2013", "v": "2013"}, {"n": "2012", "v": "2012"}, {"n": "2011", "v": "2011"}, {"n": "2010", "v": "2010"}, {"n": "2009", "v": "2009"}]}]}
+		"filter": {"CCTV":[{"key":"cid","name":"频道","value":[{"n":"全部","v":""},{"n":"CCTV-1综合","v":"EPGC1386744804340101"},{"n":"CCTV-2财经","v":"EPGC1386744804340102"},{"n":"CCTV-3综艺","v":"EPGC1386744804340103"},{"n":"CCTV-4中文国际","v":"EPGC1386744804340104"},{"n":"CCTV-5体育","v":"EPGC1386744804340107"},{"n":"CCTV-6电影","v":"EPGC1386744804340108"},{"n":"CCTV-7国防军事","v":"EPGC1386744804340109"},{"n":"CCTV-8电视剧","v":"EPGC1386744804340110"},{"n":"CCTV-9纪录","v":"EPGC1386744804340112"},{"n":"CCTV-10科教","v":"EPGC1386744804340113"},{"n":"CCTV-11戏曲","v":"EPGC1386744804340114"},{"n":"CCTV-12社会与法","v":"EPGC1386744804340115"},{"n":"CCTV-13新闻","v":"EPGC1386744804340116"},{"n":"CCTV-14少儿","v":"EPGC1386744804340117"},{"n":"CCTV-15音乐","v":"EPGC1386744804340118"},{"n":"CCTV-16奥林匹克","v":"EPGC1634630207058998"},{"n":"CCTV-17农业农村","v":"EPGC1563932742616872"},{"n":"CCTV-5+体育赛事","v":"EPGC1468294755566101"}]},{"key":"fc","name":"分类","value":[{"n":"全部","v":""},{"n":"新闻","v":"新闻"},{"n":"体育","v":"体育"},{"n":"综艺","v":"综艺"},{"n":"健康","v":"健康"},{"n":"生活","v":"生活"},{"n":"科教","v":"科教"},{"n":"经济","v":"经济"},{"n":"农业","v":"农业"},{"n":"法治","v":"法治"},{"n":"军事","v":"军事"},{"n":"少儿","v":"少儿"},{"n":"动画","v":"动画"},{"n":"纪实","v":"纪实"},{"n":"戏曲","v":"戏曲"},{"n":"音乐","v":"音乐"},{"n":"影视","v":"影视"}]},{"key":"fl","name":"字母","value":[{"n":"全部","v":""},{"n":"A","v":"A"},{"n":"B","v":"B"},{"n":"C","v":"C"},{"n":"D","v":"D"},{"n":"E","v":"E"},{"n":"F","v":"F"},{"n":"G","v":"G"},{"n":"H","v":"H"},{"n":"I","v":"I"},{"n":"J","v":"J"},{"n":"K","v":"K"},{"n":"L","v":"L"},{"n":"M","v":"M"},{"n":"N","v":"N"},{"n":"O","v":"O"},{"n":"P","v":"P"},{"n":"Q","v":"Q"},{"n":"R","v":"R"},{"n":"S","v":"S"},{"n":"T","v":"T"},{"n":"U","v":"U"},{"n":"V","v":"V"},{"n":"W","v":"W"},{"n":"X","v":"X"},{"n":"Y","v":"Y"},{"n":"Z","v":"Z"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2022","v":"2022"},{"n":"2021","v":"2021"},{"n":"2020","v":"2020"},{"n":"2019","v":"2019"},{"n":"2018","v":"2018"},{"n":"2017","v":"2017"},{"n":"2016","v":"2016"},{"n":"2015","v":"2015"},{"n":"2014","v":"2014"},{"n":"2013","v":"2013"},{"n":"2012","v":"2012"},{"n":"2011","v":"2011"},{"n":"2010","v":"2010"},{"n":"2009","v":"2009"},{"n":"2008","v":"2008"},{"n":"2007","v":"2007"},{"n":"2006","v":"2006"},{"n":"2005","v":"2005"},{"n":"2004","v":"2004"},{"n":"2003","v":"2003"},{"n":"2002","v":"2002"},{"n":"2001","v":"2001"},{"n":"2000","v":"2000"}]},{"key":"month","name":"月份","value":[{"n":"全部","v":""},{"n":"12","v":"12"},{"n":"11","v":"11"},{"n":"10","v":"10"},{"n":"09","v":"09"},{"n":"08","v":"08"},{"n":"07","v":"07"},{"n":"06","v":"06"},{"n":"05","v":"05"},{"n":"04","v":"04"},{"n":"03","v":"03"},{"n":"02","v":"02"},{"n":"01","v":"01"}]}]}
 	}
 	header = {
-		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36"
+		"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36",
+		"Origin": "https://tv.cctv.com",
+		"Referer": "https://tv.cctv.com/"
 	}
 
 	def localProxy(self,param):
